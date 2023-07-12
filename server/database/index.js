664893bc3348b208e24c275a292cfab5573762a5
Salami <medselimthabet2@gmail.com>
2023-07-12 11:06:20 +0100

    sequelize fixed

--- server/database/index.js
+++ server/database/index.js
@@ -1,8 +1,12 @@
-const Sequelize = require('sequelize');
+const { Sequelize, DataTypes } = require("sequelize");
 const sequelize = new Sequelize('bmw', 'root', 'root', {
   host: 'localhost',
   dialect: 'mysql',
 });
+const db = {};
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
 sequelize.query("CREATE DATABASE IF NOT EXISTS bmw;") // Create the database if it doesn't exist
 .then(() => {
   })
