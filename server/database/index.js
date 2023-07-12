69fc58e3a1d8e648e53fdefa2c4106b2e8b9da3d
KhouloudNEILY <neilykhouloud@gmail.com>
2023-07-12 16:56:57 +0200

    admin database

--- server/database/index.js
+++ server/database/index.js
@@ -1,20 +1,27 @@
 const { Sequelize, DataTypes } = require("sequelize");
-const sequelize = new Sequelize('bmw', 'yacine', 'yacine251203**', {
+const sequelize = new Sequelize('bmw', 'root', 'root', {
   host: 'localhost',
   dialect: 'mysql',
   logging: false,
   sync:false,
 });
-const db = {};
+const db = {};// instance of sequlize
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
 db.UsedCars = require("../database/model/usedcars")(sequelize, DataTypes);
 db.Admin = require("../database/model/admin")(sequelize, DataTypes);
-db.Seller = require("../database/model/seller")(sequelize, DataTypes);
-db.Client = require("../database/model/client")(sequelize, DataTypes);
+// db.Seller = require("../database/model/seller")(sequelize, DataTypes);
+// db.Client = require("../database/model/client")(sequelize, DataTypes);
 db.NewCars = require("../database/model/newcars")(sequelize, DataTypes);
 
+
+db.Admin.hasMany(db.NewCars)
+db.NewCars.belongsTo({
+  onDelete: 'CASCADE',
+  onUpdate: 'RESTRICT'
+
+})
 
 
 sequelize.query("CREATE DATABASE IF NOT EXISTS BMW;") // Create the database if it doesn't exist
