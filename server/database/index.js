a32209fc3e8038e86cd8f49c6eb600c814813177
yacineamrouche <yacineamrouchr03@gmail.com>
2023-07-12 15:59:00 +0100

    yacine

--- server/database/index.js
+++ server/database/index.js
@@ -10,12 +10,13 @@ db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
 db.UsedCars = require("../database/model/usedcars")(sequelize, DataTypes);
-db.Admin = require("../database/model/admin")(sequelize, DataTypes);
-db.Seller = require("../database/model/seller")(sequelize, DataTypes);
-db.Client = require("../database/model/client")(sequelize, DataTypes);
-db.NewCars = require("../database/model/newcars")(sequelize, DataTypes);
+// db.Admin = require("../database/model/admin")(sequelize, DataTypes);
+// db.Seller = require("../database/model/seller")(sequelize, DataTypes);
+// db.Client = require("../database/model/client")(sequelize, DataTypes);
+// db.NewCars = require("../database/model/newcars")(sequelize, DataTypes);
 
-
+db.UsedCars.belongsTo(db.Seller, { foreignKey: 'seller_idseller' });
+db.Seller.hasMany(db.UsedCars , { foreignKey: 'seller_idseller' })
 
 sequelize.query("CREATE DATABASE IF NOT EXISTS BMW;") // Create the database if it doesn't exist
   .then(() => {
